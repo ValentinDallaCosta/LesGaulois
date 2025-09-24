bb9e8035c92b006484d25d7baed5cbaf5e2230fd
Valentin Dalla-costa <dlv5489a@ensfsi.onmicrosoft.com>
2025-09-24 14:46:45 +0200

    ajout du package personnages

---
+++ src/personnages/Romain.java
@@ -0,0 +1,16 @@
+package personnages;
+
+public class Romain {
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	private String nom;
+	private int force;
+
+	public String getNom() {
+		return nom;
+	}
+
+}
